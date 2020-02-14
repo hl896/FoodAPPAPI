7445e563864fa3520e5762a47c3593b243840307
yeung.li <yeung.li@hinterlands.tech>
2020-02-14 19:32:46 +1100

    correct version

---
+++ build/precache-manifest.ad1842bc727cdb903214fdfca5284fa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c4c308003a988fd03e25177645069a",
+    "url": "/FoodAPPAPI/index.html"
+  },
+  {
+    "revision": "84cdb225a24758510906",
+    "url": "/FoodAPPAPI/static/css/main.d9949795.chunk.css"
+  },
+  {
+    "revision": "68c36e1938a3d40a71e1",
+    "url": "/FoodAPPAPI/static/js/2.d3a5e314.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/FoodAPPAPI/static/js/2.d3a5e314.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84cdb225a24758510906",
+    "url": "/FoodAPPAPI/static/js/main.b963276c.chunk.js"
+  },
+  {
+    "revision": "f04c281e44df64c730ce",
+    "url": "/FoodAPPAPI/static/js/runtime-main.d56e5d43.js"
+  }
+]);
